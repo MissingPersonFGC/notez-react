b237d50bd2ee35339a280a64f9969880770dd737
Corey Lanier <iam@coreylanier.com>
2018-06-10 22:44:50 -0400

    finished login/register routing, now doing firebase

--- dev/scripts/Login.js
+++ dev/scripts/Login.js
@@ -11,8 +11,19 @@ import firebase from "firebase";
 class Login extends React.Component {
     render() {
         return(
-            <div className="user-header">
-                <h1>NoteZ <span className="smaller-heading"> - Login</span></h1>
+            <div>
+                <div className="user-header">
+                    <h1>NoteZ <span className="smaller-heading"> - Login</span></h1>
+                </div>
+                <div className="user-form">
+                    <form className="login-form">
+                        <input type="email" name="email" required placeholder="Email Address" />
+                        <input type="password" name="password" required placeholder="Password" />
+                        <input type="submit" name="login" value="Sign-in" />
+                        <Link to="/register">Sign-up</Link>
+                        <Link to="/">Home</Link>
+                    </form>
+                </div>
             </div>
         )
     }
--- dev/scripts/Register.js
+++ dev/scripts/Register.js
@@ -11,8 +11,21 @@ import firebase from "firebase";
 class Register extends React.Component {
     render() {
         return(
-            <div className="user-header">
-                <h1>NoteZ <span className="smaller-heading"> - User Registration</span></h1>
+            <div>
+                <div className="user-header">
+                    <h1>NoteZ <span className="smaller-heading"> - User Registration</span></h1>
+                </div>
+                <div className="user-form">
+                    <form className="user-register">
+                        <input type="text" name="username" placeholder="Username"/>
+                        <input type="email" name="email" placeholder="Email Address"/>
+                        <input type="password" name="password" placeholder="Password"/>
+                        <input type="password" name="password-verify" placeholder="Verify Password"/>
+                        <input type="submit" value="Sign-up"/>
+                        <Link to="/login">Sign-in</Link>
+                        <Link to="/">Home</Link>
+                    </form>
+                </div>
             </div>
         )
     }
