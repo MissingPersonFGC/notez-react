1926ce01ee676cee9699974329d76854a531fdb5
Corey Lanier <iam@coreylanier.com>
2018-06-11 23:52:04 -0400

    login now sorta working

--- dev/scripts/FindNotes.js
+++ dev/scripts/FindNotes.js
@@ -4,10 +4,36 @@ import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom"
 import firebase from 'firebase';
 
 class FindNotes extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            loggedIn: true
+        };
+        this.doLogout = this.doLogout.bind(this);
+    }
+
+    doLogout(e) {
+        firebase.auth().signOut().then((res) => {
+            this.setState({
+                loggedIn: false
+            });
+        });
+    }
+    
     render() {
         return(
-            <div>
-                
+            <div className="notes-header">
+                <div className="container">
+                    <div className="notes-header__heading">
+                        <h1>NoteZ</h1>
+                    </div>
+                    <div className="notes-header__user">
+                        <p>Welcome, username!</p>
+                        <div className="notes-header__user__button">
+                            <button onClick={this.doLogout}>Sign-out</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- dev/scripts/Login.js
+++ dev/scripts/Login.js
@@ -9,6 +9,52 @@ import {
 import firebase from "firebase";
 
 class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            loginEmail: '',
+            loginPassword: ''
+        }
+        this.doLogin = this.doLogin.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+    }
+
+    setEmail(e) {
+        this.setState({
+            loginEmail: e.target.value
+        });
+    }
+
+    setPassword(e) {
+        this.setState({
+            loginPassword: e.target.value
+        });
+    }
+
+    doLogin(e) {
+        e.preventDefault();
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailCheck = re.test(String(this.state.loginEmail.toLowerCase()));
+        if (this.state.loginEmail !== '' && emailCheck === true && this.state.loginPassword !== '') {
+            const email = this.state.loginEmail;
+            const password = this.state.loginPassword;
+            firebase.auth().signInWithEmailAndPassword(email, password).then((success) => {
+                console.log(`Logged in as ${success.user.email}`);
+            }), (error) => {
+                console.log(error);
+            }
+
+            this.setState({
+                loginEmail: '',
+                loginPassword: '',
+                loggedIn: true
+            })
+        } else {
+            alert(`Please enter valid login credentials.`);
+        }
+    }
+
     render() {
         return(
             <div>
@@ -17,9 +63,9 @@ class Login extends React.Component {
                 </div>
                 <div className="user-form">
                     <form className="login-form">
-                        <input type="email" name="email" required placeholder="Email Address" />
-                        <input type="password" name="password" required placeholder="Password" />
-                        <input type="submit" name="login" value="Sign-in" />
+                        <input type="email" name="email" required placeholder="Email Address" onChange={this.setEmail}/>
+                        <input type="password" name="password" required placeholder="Password" onChange={this.setPassword} />
+                        <input type="submit" name="login" value="Sign-in" onClick={this.doLogin} />
                         <Link to="/register">Sign-up</Link>
                         <Link to="/">Home</Link>
                     </form>
